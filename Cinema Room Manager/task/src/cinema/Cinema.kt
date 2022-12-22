194924a51e3c2ca725fee56c03deae776beffca9
Eder Martinez <edermtz.04@gmail.com>
2022-12-22 07:34:37 -0600

    First Commit with the first Chapter finished

---
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -0,0 +1,14 @@
+package cinema
+
+fun main() {
+    val row: String = "S S S S S S S S"
+    println("Cinema:")
+    println("  1 2 3 4 5 6 7 8")
+    println("1 $row")
+    println("2 $row")
+    println("3 $row")
+    println("4 $row")
+    println("5 $row")
+    println("6 $row")
+    println("7 $row")
+}
