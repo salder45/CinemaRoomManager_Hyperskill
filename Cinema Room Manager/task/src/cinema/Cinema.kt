e5c770046ebc12165bd8a39da165cbb53d93454f
Eder Martinez <edermtz.04@gmail.com>
2022-12-23 08:05:28 -0600

    Finish Step 2

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,6 +1,20 @@
 package cinema
 
 fun main() {
+    println("Enter the number of rows:")
+    val rows: Int = readln().toInt()
+    println("Enter the number of seats in each row:")
+    val seatsPerRow: Int = readln().toInt()
+    val totalSeats: Int = rows * seatsPerRow
+    var income: Int = totalSeats * 10
+    if (totalSeats > 60) {
+        val frontHalf: Int = rows / 2
+        val backHalf: Int = rows - frontHalf
+        income = frontHalf * seatsPerRow * 10 + backHalf * seatsPerRow * 8
+    }
+    println("Total income:")
+    println("$$income")
+    /*
     val row: String = "S S S S S S S S"
     println("Cinema:")
     println("  1 2 3 4 5 6 7 8")
@@ -11,4 +25,5 @@ fun main() {
     println("5 $row")
     println("6 $row")
     println("7 $row")
+     */
 }
