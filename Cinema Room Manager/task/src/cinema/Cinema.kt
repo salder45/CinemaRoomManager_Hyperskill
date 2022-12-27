2b5b21c5f188b9acb9ced7bc887a3853833ee332
Eder Martinez <edermtz.04@gmail.com>
2022-12-27 09:45:44 -0600

    Stage 3

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -2,28 +2,42 @@ package cinema
 
 fun main() {
     println("Enter the number of rows:")
-    val rows: Int = readln().toInt()
+    val rowsAtCinema: Int = readln().toInt()
     println("Enter the number of seats in each row:")
     val seatsPerRow: Int = readln().toInt()
-    val totalSeats: Int = rows * seatsPerRow
-    var income: Int = totalSeats * 10
-    if (totalSeats > 60) {
-        val frontHalf: Int = rows / 2
-        val backHalf: Int = rows - frontHalf
-        income = frontHalf * seatsPerRow * 10 + backHalf * seatsPerRow * 8
+    val cinema: MutableList<MutableList<String>> = mutableListOf()
+    val header = mutableListOf(" ")
+    for (headerColumn in 1..seatsPerRow) {
+        header.add(headerColumn.toString())
     }
-    println("Total income:")
-    println("$$income")
-    /*
-    val row: String = "S S S S S S S S"
+    cinema.add(header)
+    for (row in 1..rowsAtCinema) {
+        val newRow = mutableListOf<String>(row.toString())
+        for (column in 1..seatsPerRow) {
+            newRow.add("S")
+        }
+        cinema.add(newRow)
+    }
     println("Cinema:")
-    println("  1 2 3 4 5 6 7 8")
-    println("1 $row")
-    println("2 $row")
-    println("3 $row")
-    println("4 $row")
-    println("5 $row")
-    println("6 $row")
-    println("7 $row")
-     */
+    for (row in 0 until cinema.size) {
+        println(cinema[row].joinToString(" "))
+    }
+    println("Enter a row number:")
+    val selectedRow: Int = readln().toInt()
+    println("Enter a seat number in that row:")
+    val selectedSeat: Int = readln().toInt()
+    val totalCinemaSeats = rowsAtCinema * seatsPerRow
+    var priceTicket: Int = 10
+    if (totalCinemaSeats > 60) {
+        val firstHalf: Int = rowsAtCinema / 2
+        if (selectedRow > firstHalf) {
+            priceTicket = 8
+        }
+    }
+    println("Ticket price: $$priceTicket")
+    cinema[selectedRow][selectedSeat] = "B"
+    println("Cinema:")
+    for (row in 0 until cinema.size) {
+        println(cinema[row].joinToString(" "))
+    }
 }
